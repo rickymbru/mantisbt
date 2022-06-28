c4a9bd5d31ce3b04b3720706cb4a8309742a50f3
rodrigolima.ciee <estagio.bi.1@cedae.com.br>
2022-06-28 15:32:45 -0300

    up mantis

---
+++ mantisbt/custom_constants_inc.php
@@ -0,0 +1,10 @@
+<?
+  define ( 'REQUIREMENTS', 21); // add requisitos
+  define ( 'VIABILITY', 31); // add requisitos
+  define ( 'DATAMART', 41); // add requisitos
+  define ( 'CUBOLAP', 51 ); // add CUBOLAP
+  define ( 'DATAVIZ', 61 ); // add DATAVIZ
+  define ( 'HOMOLOGATION', 71 ); // add DATAVIZ
+  define ( 'CANCELLED', 101 );   // add cancelado
+
+?>
